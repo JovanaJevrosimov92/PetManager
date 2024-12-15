13f6d51763ae1d7c747ca66ea54010813847ba6b
JovanaJevrosimov <jovana.jevrosimov@outlook.com>
2024-12-15 02:56:25 +0100

    Dodate su sesije i zapocet rad sa dodavanjem ljubimca, druga verzija

--- src/main/java/PetAppBackend/config/CorsConfig.java
+++ src/main/java/PetAppBackend/config/CorsConfig.java
@@ -13,6 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("http://localhost:8080")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .exposedHeaders("Set-Cookie")
                         .allowedMethods("GET", "POST", "DELETE");
             }
         };
